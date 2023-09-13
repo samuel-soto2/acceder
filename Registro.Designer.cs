f25a27102a34ad6036f3d31687ae60efc2eae7f4
samuel-soto2 <1461888@senati.pe>
2023-09-13 16:35:38 -0500

    aqui ya tenemos el otro formulario de registro listo, con boton de guardar

--- Registro.Designer.cs
+++ Registro.Designer.cs
@@ -33,6 +33,9 @@
             cerrar2 = new Button();
             n_user = new TextBox();
             n_pass = new TextBox();
+            guardar = new Button();
+            label1 = new Label();
+            label2 = new Label();
             panel1.SuspendLayout();
             SuspendLayout();
             // 
@@ -82,12 +85,53 @@
             n_pass.TabIndex = 2;
             n_pass.TextAlign = HorizontalAlignment.Center;
             // 
+            // guardar
+            // 
+            guardar.BackColor = Color.OrangeRed;
+            guardar.FlatAppearance.BorderColor = Color.White;
+            guardar.FlatAppearance.BorderSize = 0;
+            guardar.FlatAppearance.MouseDownBackColor = Color.White;
+            guardar.FlatAppearance.MouseOverBackColor = Color.White;
+            guardar.FlatStyle = FlatStyle.Flat;
+            guardar.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            guardar.Location = new Point(141, 195);
+            guardar.Name = "guardar";
+            guardar.Size = new Size(90, 35);
+            guardar.TabIndex = 3;
+            guardar.Text = "Guardar";
+            guardar.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.Control;
+            label1.Font = new Font("Yu Gothic UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(40, 77);
+            label1.Name = "label1";
+            label1.Size = new Size(64, 21);
+            label1.TabIndex = 7;
+            label1.Text = "Usuario";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = SystemColors.Control;
+            label2.Font = new Font("Yu Gothic UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(28, 139);
+            label2.Name = "label2";
+            label2.Size = new Size(89, 21);
+            label2.TabIndex = 8;
+            label2.Text = "Contraseña";
+            // 
             // Registro
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
             ClientSize = new Size(382, 263);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(guardar);
             Controls.Add(n_pass);
             Controls.Add(n_user);
             Controls.Add(panel1);
@@ -105,5 +149,8 @@
         private Button cerrar2;
         private TextBox n_user;
         private TextBox n_pass;
+        private Button guardar;
+        private Label label1;
+        private Label label2;
     }
 }
