9d2ceadc2ab70c4d3ae92b6fba7b3b06914e5fe9
samuel-soto2 <1461888@senati.pe>
2023-09-13 17:30:04 -0500

    si en caso sale error nos tendriamos que registrar y nos saldria el mensaje de ususario registrado

--- Registro.cs
+++ Registro.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace acceso
 {
@@ -19,7 +20,31 @@ namespace acceso
 
         private void guardar_Click(object sender, EventArgs e)
         {
+            MySqlConnection con = new MySqlConnection("server - 127.0.0.1 ; Database = login; user Id-root ; password-lentp123 ");
+            try
+            {
+                con.Open();
+
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Error" + ex.ToString());
+                throw;
+            }
+            string sql = "isert into users(user,pass) values ('" + n_user.Text + "' , '" + n_pass.Text + "')";
+            MySqlCommand cmd = new MySqlCommand(sql, con);
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("USUARIO REGISTRADO");
+                this.Hide();
+                Form1 n1 = new Form1();
+                n1.Show();
+            }
+            catch (MySqlException ex) { MessageBox.Show("Error " + ex.ToString()); }
 
         }
+
+    }
     }
 }
